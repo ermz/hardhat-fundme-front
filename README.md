c4fc90b45aa0b704d8d8e8a693ad1a40628a5083
ermz <eramirez4369@gmail.com>
2022-08-20 01:26:46 -0400

    Simple front end with connection to original FundMe.sol contract w/t everything added

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple HTML/CSS for FundMe project
---
+++ index.js
@@ -0,0 +1,34 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        window.ethereum.request({method: "eth_requestAccounts"})
+        connectButton.innerHTML = "Connected!"
+    } else {
+        connectButton.innerHTML = "Please install metamask"
+    }
+}
+
+async function fund() {
+    const ethAmount = "77"
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
